3a81186700555a590cc8d52261a6a2532f718fab
lucasmsilva <lucas.silva16@unifesp.br>
2024-12-12 17:41:11 -0300

    feat: add rdt library

---
+++ rdt/rdt_2.h
@@ -0,0 +1,18 @@
+#ifndef _RDT_2_H_
+#define _RDT_2_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define MAX_MSG_LEN 1000
+#define ERROR -1
+#define TRUE 1
+#define FALSE 0
+#define SUCCESS 0
+
+int biterror_inject = FALSE;
+
+#endif
