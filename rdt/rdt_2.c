fddc2907baf953b461a5ea025af2a7b6a15fe46a
Fernando Daniel <fernando.dan98@gmail.com>
2024-12-13 11:15:34 -0300

    Adicionando material de apoio para a atividade e alguns comentários em "rdt_2.c"

--- rdt/rdt_2.c
+++ rdt/rdt_2.c
@@ -48,16 +48,21 @@ int rdt_send(int sockfd, void *buf, int buf_len, struct sockaddr_in *dst) {
 	if (biterror_inject) {
 		memset(p.msg, 0, MAX_MSG_LEN);
 	}
+
+	// Enviando o pacote; retorna o número de bytes enviados. Se -1, erro no envio.
 	ns = sendto(sockfd, &p, p.h.pkt_size, 0,
 			(struct sockaddr *)dst, sizeof(struct sockaddr_in));
 	if (ns < 0) {
 		perror("sendto():");
 		return ERROR;
 	}
+
 	// recvfrom ACK ou NAK
+
 	return ns;
 }
 
+// Retorna TRUE (1) se o checksum do pacote estiver corrompido. Senão, FALSE (0).
 int iscorrupted(pkt pr){
 	pkt pl = pr;
 	pl.h.csum = 0;
@@ -75,24 +80,37 @@ int rdt_recv(int sockfd, void *buf, int buf_len, struct sockaddr_in *src) {
 	int addrlen = sizeof(struct sockaddr_in);
 	memset(&p, 0, sizeof(hdr));
 
+	// Recebendo o pacote; retorna o número de bytes recebidos. Se -1, erro no recebimento.
 	nr = recvfrom(sockfd, &p, sizeof(pkt), 0, (struct sockaddr*)src,
 		(socklen_t *)&addrlen);
 	if (nr < 0) {
 		perror("recvfrom():");
 		return ERROR;
 	}
+
+	// Verificando se o pacote está corrompido. Se estiver, deve-se enviar NAK.
 	if (iscorrupted(p)) {
 		printf("checksum: pacote corrompido. \n");
-		// enviar NAK
+		
+		// Enviar NAK
+
 		return ERROR;
 	}
+
+	// Caso o pacote não foi corrompido (checksum é igual ao calculado pelo remetente).
 	int msg_size = p.h.pkt_size - sizeof(hdr);
+
+	// Verificando se o tamanho do buffer é suficiente para a mensagem, pois o buffer possui um limite no tamanho da mensagem.
 	if (msg_size > buf_len) {
 		printf("rdt_rcv(): tamanho insuficiente de buf (%d) para payload (%d).\n", 
 			buf_len, msg_size);
 		return ERROR;
 	}
+
+	// Caso o tamanho do buffer seja suficiente, copia a mensagem para o buffer.
 	memcpy(buf, p.msg, msg_size);
-	// enviar ACK
+
+	// Enviar ACK
+
 	return p.h.pkt_size - sizeof(hdr);
 }
